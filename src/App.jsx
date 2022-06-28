0c47f03e938d8100daf5f7d080f77593458005de
damian59 <damianalmanza89@gmail.com>
2022-06-27 21:46:57 -0300

    subiendo primeros progresos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <main>
+        TicTOC Clone
+      </main>
+    </div>
+  )
+}
+
+export default App
